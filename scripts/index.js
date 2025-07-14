f9fd3c3b9b19cf23894b96de1af24cfc976f9697
Daniel <dvrdirect@gmail.com>
2025-07-14 10:24:48 -0600

    Se crearon varias funciones en JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Boton de editar y cerrar perfil
 let popup = document.querySelector(".popup");
 let editBtn = document.querySelector(".profile__edit-btn");
 let closeBtn = popup.querySelector(".popup__close");
@@ -12,6 +13,7 @@ function closePopup() {
 editBtn.addEventListener("click", openPopup);
 closeBtn.addEventListener("click", closePopup);
 
+// Cambiar los datos de la profile-form
 let profileName = document.querySelector("#profile-name");
 let profileOcp = document.querySelector("#profile-ocupation");
 
@@ -28,3 +30,146 @@ function profileSubmit(evt) {
 
 formElement.addEventListener("submit", profileSubmit);
 formElement.addEventListener("submit", closePopup);
+
+//Creación de tarjetas
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+    gridArea: "elements-1",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+    gridArea: "elements-2",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+    gridArea: "elements-3",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+    gridArea: "elements-4",
+  },
+  {
+    name: "Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+    gridArea: "elements-5",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+    gridArea: "elements-6",
+  },
+];
+
+const container = document.querySelector(".elements__container");
+initialCards.forEach((data) => {
+  const card = document.createElement("div");
+  card.classList.add("elements__card");
+  card.style.gridArea = data.gridArea;
+
+  card.innerHTML = `
+  <div class="elements__image-container">
+  <img
+      class="elements__rectangle"
+      src="./images/Rectangle.png"
+      alt="Decoración"
+    />
+  <img src="${data.link}" alt"${data.name}" class="elements__pic"/>
+  </div>
+  <div class="elements__text-box">
+    <p class="elements__text">${data.name}</p>
+    <button class="elements__like-btn"></button>
+  </div>
+`;
+  container.appendChild(card);
+});
+
+// Creación de form para agregar cards
+
+const cardFormClose = document.querySelector("#cerrar");
+const addButton = document.querySelector(".profile__add-btn");
+const cardForm = document.querySelector(".card__form");
+const showDisplay = () => {
+  cardForm.classList.remove("card__form");
+  cardForm.classList.add("card__form-active");
+};
+function removeDisplay() {
+  cardForm.classList.remove("card__form-active");
+  cardForm.classList.add("card__form");
+}
+addButton.addEventListener("click", showDisplay);
+cardFormClose.addEventListener("click", removeDisplay);
+
+//Agregar tarjetas
+const addTitle = cardForm.querySelector(".card__add-title");
+const addLink = cardForm.querySelector(".card__form-link");
+const createBtn = cardForm.querySelector(".card__form-button");
+
+function renderCard(data) {
+  const card = document.createElement("div");
+  card.classList.add("elements__card");
+  card.style.gridArea = data.gridArea;
+
+  card.innerHTML = `
+  <div class="elements__image-container">
+  <img
+      class="elements__rectangle"
+      src="./images/Rectangle.png"
+      alt="Decoración"
+    />
+  <img src="${data.link}" alt"${data.name}" class="elements__pic"/>
+  </div>
+  <div class="elements__text-box">
+    <p class="elements__text">${data.name}</p>
+    <button class="elements__like-btn"></button>
+  </div>
+`;
+  const likeButton = card.querySelector(".elements__like-btn");
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("elements__like-btn_active");
+  });
+
+  container.appendChild(card);
+}
+
+createBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const newName = addTitle.value.trim();
+  const newLink = addLink.value.trim();
+
+  if (newName && newLink) {
+    const newCard = {
+      name: newName,
+      link: newLink,
+      gridArea: "elements-1",
+    };
+
+    initialCards.unshift(newCard);
+
+    initialCards.forEach((card, index) => {
+      card.gridArea = `elements-${index + 1}`;
+    });
+    renderCard(newCard);
+    console.log("Nuevo card agregado:", newCard);
+
+    cardForm.reset();
+    removeDisplay();
+  } else {
+    console.log("Faltan datos en el formulario");
+  }
+});
+
+//Like button
+
+const likeBtn = document.querySelectorAll(".elements__like-btn");
+likeBtn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    btn.classList.toggle("elements__like-btn_active");
+  });
+});
